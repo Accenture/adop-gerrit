e22b219958eaa287dfa7805d04d70062e6a7217b
Nick Griffin <nicholas.griffin@accenture.com>
2015-08-07 19:15:44 +0100

    DA-452: Adding admin create script

---
+++ resources/scripts/create_admin_user.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+set -e
+
+# Usage
+usage() {
+    echo "Usage:"
+    echo "    ${0} -u <USER> -p <PASSWORD>"
+    exit 1
+}
+
+# Constants
+SLEEP_TIME=5
+
+while getopts "c:p:k:u:" opt; do
+  case $opt in
+    u)
+      username=${OPTARG}
+      ;;
+    p)
+      password=${OPTARG}
+      ;;
+    *)
+      echo "Invalid parameter(s) or option(s)."
+      usage
+      ;;
+  esac
+done
+
+if [ -z "${username}" ] || [ -z "${password}" ]; then
+    echo "Parameters missing"
+    usage
+fi
+
+echo "Testing Gerrit Connection"
+until curl -sL -w "%{http_code}\\n" "http://localhost:8080/gerrit/login/%23/q/status:open" -o /dev/null | grep "401" &> /dev/null
+do
+    echo "Gerrit unavailable, sleeping for ${SLEEP_TIME}"
+    sleep "${SLEEP_TIME}"
+done
+
+curl -X POST --data "username=${username}&password=${password}" http://localhost:8080/gerrit/login/%23/q/status:open
