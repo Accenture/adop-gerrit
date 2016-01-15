35c3fd114dbcbfc909136680c3209670490e770b
Nick Griffin <nicholas.griffin@accenture.com>
2016-01-15 13:57:02 +0000

    Switching Jenkins from Non-Interactive to Administrator

--- resources/scripts/gerrit_init.sh
+++ resources/scripts/gerrit_init.sh
@@ -5,7 +5,7 @@ set -e
 /var/gerrit/adop\_scripts/create\_user.sh -u gerrit -p gerrit
 /var/gerrit/adop\_scripts/create\_user.sh -u jenkins -p jenkins
 /var/gerrit/adop\_scripts/create\_user.sh -u john.smith -p Password01
-/var/gerrit/adop\_scripts/add\_user\_to\_group.sh -A gerrit -P gerrit -u jenkins -g "Non-Interactive Users"
+/var/gerrit/adop\_scripts/add\_user\_to\_group.sh -A gerrit -P gerrit -u jenkins -g "Administrators"
 /var/gerrit/adop\_scripts/add\_user\_to\_group.sh -A gerrit -P gerrit -u john.smith -g Administrators
 
 exit 0
